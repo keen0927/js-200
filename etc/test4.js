fd6633f5e79a83838c83da519259b9d0b5497595
keen <kyh0927@within.co.kr>
2019-10-17 18:39:00 +0900

    19101~7

---
+++ etc/test4.js
@@ -0,0 +1,45 @@
+// animal / dog 클래스 생성
+// animal 은 2개의 매개변수를 받는다 name / type
+// 공개속성으로 설정
+// 추가로 sayName과 sayAnimalType이라는 두 개의 함수가 있어야한다
+// sayName은 name을 출력
+// sayAnimalType은 animalType을 출력한다
+// 두 속성 모두 생성장에서 초깃값이 설정된다
+
+// Dog클래스는 animal을 상속한다
+
+
+function Animal(name, type) {
+  this.name = name;
+  this.type = type;
+}
+
+Animal.prototype.sayName = function() {
+  console.log(this.name);
+}
+Animal.prototype.sayType = function() {
+  console.log(this.type);
+}
+
+let kkk = new Animal('hatnim','cat');
+
+kkk.sayName();
+kkk.sayType();
+
+function Dog(name) {
+  Animal.call(this, name, 'Dog');
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+
+
+function callMe(number) {
+  if ( number === 0) {
+    return;
+  } else {
+    console.log(number);
+    callMe(number - 1);
+  }
+}
+
+callMe(10);
