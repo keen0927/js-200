aa79448b04d09590d8eaf9b4018b3a0d9962d45c
keen <kyh0927@within.co.kr>
2019-11-12 09:11:40 +0900

    191112

--- structure/02.js
+++ structure/02.js
@@ -1,19 +1,38 @@
-const arr = [1,2,3,4,5,6,7,8,9,10];
+// const arr = [1,2,3,4,5,6,7,8,9,10];
 
-function search(arr, n) {
-  var lowIndex = 0, highIndex = arr.length - 1;
+// function search(arr, n) {
+//   var lowIndex = 0, highIndex = arr.length - 1;
+
+//   while(lowIndex <= highIndex) {
+//     var midIndex = Math.floor((highIndex+lowIndex) / 2);
+//     if (arr[midIndex] === n) {
+//       return midIndex;
+//     } else if (n > arr[midIndex]) {
+//       lowIndex = midIndex;
+//     } else {
+//       highIndex = midIndex;
+//     }
+//   }
+//   return - 1;
+// }
+
+// console.log(search(arr,5));
+
+function searchBinary(arr, num) {
+  var lowIndex = 0,
+      highIndex = arr.length - 1;
 
   while(lowIndex <= highIndex) {
-    var midIndex = Math.floor((highIndex+lowIndex) / 2);
-    if (arr[midIndex] === n) {
-      return midIndex;
-    } else if (n > arr[midIndex]) {
-      lowIndex = midIndex;
+    var midIndex = Math.floor( (highIndex + lowIndex) / 2);
+    if (arr[midIndex] == num) {
+      return midIndex
+    } else if ( num > arr[midIndex]) {
+      lowIndex = midIndex
     } else {
-      highIndex = midIndex;
+      highIndex = midIndex
     }
   }
-  return - 1;
+
+  return -1;
 }
 
-console.log(search(arr,5));
