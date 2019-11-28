b3341b0040f54414c4e127c4419c9d3c4d8cc9b7
keen <kyh0927@within.co.kr>
2019-11-28 13:52:40 +0900

    191128

--- inflearn/es6-inflearn/src/main.js
+++ inflearn/es6-inflearn/src/main.js
@@ -1,6 +1,29 @@
 class Blog {
   constructor() {
     console.log('Blog Start');
+    const dataURL = "http://localhost:3000/api"
+    this.setInitData(dataURL);
+  }
+
+  setInitData(dataURL) {
+    this.getData(dataURL);
+  }
+
+  getData(dataURL) {
+    const oReq = new XMLHttpRequest();
+
+    oReq.addEventListener('load', () => {
+      const list = JSON.parse(oReq.responseText);
+
+      const listBeears = list.beers;
+
+      listBeears.forEach( v => {
+        console.log(v.name);
+      })
+    });
+
+    oReq.open('GET',dataURL);
+    oReq.send();
   }
 }
 
