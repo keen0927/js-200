b55ddc38ee65e67b6de22622c992ee225fa985df
keen <kyh0927@within.co.kr>
2020-02-27 13:46:38 +0900

    20200227

--- test2.js
+++ test2.js
@@ -1,13 +1,13 @@
-let numbers = [3, 1, 4, 1, 5, 9];
+// let numbers = [3, 1, 4, 1, 5, 9];
 
-function recursive(acc, array) {
-    if (array.length === 0) {
-        return acc
-    } else {
-        return recursive(acc + array[0], array.slice(1));
-    }
-}
+// function recursive(acc, array) {
+//     if (array.length === 0) {
+//         return acc
+//     } else {
+//         return recursive(acc + array[0], array.slice(1));
+//     }
+// }
 
 
 
-console.log(recursive(0, numbers));
+// console.log(recursive(0, numbers));
